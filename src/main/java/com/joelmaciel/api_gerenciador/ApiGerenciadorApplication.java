15b11cdc59a1da73aea9dfe182ea2a6a3e2998d5
Joel <joel.viana@audora.com.br>
2024-08-20 20:07:42 -0300

    commit inicial

---
+++ src/main/java/com/joelmaciel/api_gerenciador/ApiGerenciadorApplication.java
@@ -0,0 +1,13 @@
+package com.joelmaciel.api_gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joelmaciel/api_gerenciador/ApiGerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelmaciel.api_gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
